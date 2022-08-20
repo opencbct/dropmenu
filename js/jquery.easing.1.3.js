d149f7498bde6b7dc256a1619bef344ea4db1497
Benjamin Kim <84877261+opencbct@users.noreply.github.com>
2022-08-20 14:59:27 -0500

    jquery

--- js/jquery.easing.1.3.js
+++ js/jquery.easing.1.3.js
@@ -114,7 +114,24 @@ jQuary.extend( jQuary.easing,
         if ((t/=d/2) < 1) return c/2*(*t*t(((s*=(1.525))+1)*t -s)) + b;
         return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
     }
-    
+    easeInBounce: function (x, t, b, c, d, s) {
+        return c - jQuery.easing.easeOutBounce (x, d-t, 0, c, d) + b; 
+    }
+    easeOutBounce: function (x, t, b, c, d) {
+        if ((t/=d) < (1/2.75)) {
+            return c*(7.5625*t*t) + b;
+        } else if (t < (2/2.75)) {
+            return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+        } else if (t < (2.5/2.75)) {
+            return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+        } else {
+            return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+    },
+    elseInOutBounce: function (x, t, b, c, d) {
+        if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
+        return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
+    }
+});
 
 
 
